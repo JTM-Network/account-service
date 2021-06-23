79a113b198de624124a2955ad07cc7798c4ead59
Joe Matthews <joetymatthews@gmail.com>
2021-06-23 11:44:43 +0100

    update test suite & add environment variables to config

--- src/test/kotlin/com/jtm/account/AccountServiceTestSuite.kt
+++ src/test/kotlin/com/jtm/account/AccountServiceTestSuite.kt
@@ -1,6 +1,8 @@
 package com.jtm.account
 
+import com.jtm.account.data.service.AccountProfileServiceTest
 import com.jtm.account.data.service.RoleServiceTest
+import com.jtm.account.presenter.controller.AuthControllerTest
 import com.jtm.account.presenter.controller.RoleControllerTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
@@ -8,6 +10,9 @@ import org.junit.runners.Suite
 @RunWith(Suite::class)
 @Suite.SuiteClasses(value = [
     RoleServiceTest::class,
-    RoleControllerTest::class
+    RoleControllerTest::class,
+
+    AccountProfileServiceTest::class,
+    AuthControllerTest::class
 ])
 class AccountServiceTestSuite
