39fe74ce2b80005f73103eac4c27b1c64b825a56
Joe Matthews <joetymatthews@gmail.com>
2021-06-23 11:53:55 +0100

    fix: multiple cookies set on Set-Cookie header

--- src/main/kotlin/com/jtm/account/data/service/AccountProfileService.kt
+++ src/main/kotlin/com/jtm/account/data/service/AccountProfileService.kt
@@ -50,8 +50,7 @@ class AccountProfileService @Autowired constructor(private val profileRepository
             .switchIfEmpty(Mono.defer { Mono.error { InvalidEmailOrPass() } })
             .flatMap {
                 if (!it.passwordMatches(password, tokenProvider.passwordEncoder())) return@flatMap Mono.error { InvalidEmailOrPass() }
-                response.headers.set("Set-Cookie", "refreshToken=" + tokenProvider.createRefreshToken(it) + ";Max-Age=5184000000;SameSite=None; HttpOnly; Path=/; Secure")
-                response.headers.set("Set-Cookie", "accessToken=" + tokenProvider.createAccessCookieToken(it) + ";Max-Age=600000;SameSite=None; HttpOnly; Path=/; Secure")
+                response.headers.set("Set-Cookie", "refreshToken=" + tokenProvider.createRefreshToken(it) + ";Max-Age=5184000000;SameSite=None; HttpOnly; Path=/; Secure, accessToken=" + tokenProvider.createAccessCookieToken(it) + ";Max-Age=600000;SameSite=None; HttpOnly; Path=/; Secure")
                 return@flatMap Mono.just(tokenProvider.createAccessToken(it))
             }
     }
