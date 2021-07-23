be8b70b5538a9f793e6508e4c41bd1886fd0edfd
Joe Matthews <joetymatthews@gmail.com>
2021-07-23 22:29:15 +0100

    add log message when added roles

--- src/main/kotlin/com/jtm/account/data/service/account/RoleService.kt
+++ src/main/kotlin/com/jtm/account/data/service/account/RoleService.kt
@@ -5,23 +5,27 @@ import com.jtm.account.core.domain.entity.Role
 import com.jtm.account.core.domain.exception.RoleFound
 import com.jtm.account.core.domain.exception.RoleNotFound
 import com.jtm.account.core.usecase.repository.RoleRepository
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.util.*
 import javax.annotation.PostConstruct
+import kotlin.math.log
 
 @Service
 class RoleService @Autowired constructor(private val roleRepository: RoleRepository) {
 
+    private val logger = LoggerFactory.getLogger(RoleService::class.java)
+
     @PostConstruct
     fun init() {
         roleRepository.findByPriority(0)
-            .switchIfEmpty(Mono.defer { roleRepository.save(Role(name = "CLIENT", priority = 0)) })
+            .switchIfEmpty(Mono.defer { roleRepository.save(Role(name = "CLIENT", priority = 0)).doOnSuccess { logger.info("Added CLIENT role.") } })
 
         roleRepository.findByPriority(10)
-            .switchIfEmpty(Mono.defer { roleRepository.save(Role(name = "ADMIN", priority = 10)) })
+            .switchIfEmpty(Mono.defer { roleRepository.save(Role(name = "ADMIN", priority = 10)).doOnSuccess { logger.info("Added ADMIN role.") } })
     }
 
     fun insertRole(role: RoleDto): Mono<Role> {
