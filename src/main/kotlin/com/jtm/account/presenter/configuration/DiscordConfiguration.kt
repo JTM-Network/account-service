faa7ffb34feeb69565942a01f54a1793461d8250
Joe Matthews <joetymatthews@gmail.com>
2021-12-14 15:05:22 +0000

    refactor: add activity & status

--- src/main/kotlin/com/jtm/account/presenter/configuration/DiscordConfiguration.kt
+++ src/main/kotlin/com/jtm/account/presenter/configuration/DiscordConfiguration.kt
@@ -3,6 +3,8 @@ package com.jtm.account.presenter.configuration
 import com.jtm.account.presenter.commands.AuthCommands
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.OnlineStatus
+import net.dv8tion.jda.api.entities.Activity
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -16,6 +18,8 @@ open class DiscordConfiguration {
     @Bean
     open fun discordBot(): JDA {
         val builder = JDABuilder.createDefault(token)
+        builder.setActivity(Activity.playing("JTM Network"))
+        builder.setStatus(OnlineStatus.IDLE)
         builder.addEventListeners(AuthCommands())
         val jda = builder.build()
         jda.upsertCommand("auth", "Authenticate discord account to JTM Network account.")
